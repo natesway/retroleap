2c09f50a8321db7cedda76c598f9e2b2ccbbeae3
Forrest Marvin <forrest.marvin@gmail.com>
2018-12-09 13:56:39 -0700

    Add detection of leapstergs vs leappad2 when starting retroarch.

--- board/leapfrog/leapstergs/fsoverlay/usr/bin/respawn_retroarch.sh
+++ board/leapfrog/leapstergs/fsoverlay/usr/bin/respawn_retroarch.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+CONFIG=/configs/retroarch.cfg.leapstergs
+
+valencia=$(dmesg | grep valencia)
+if [[ $? == 0 ]]; then
+    CONFIG=/configs/retroarch.cfg.leappad2
+fi
+
 while `true`
 do
   echo 0 > /sys/devices/platform/lf2000-fb.0/graphics/fb0/blank
   export SDL_NOMOUSE=1
-  retroarch -v -c /configs/retroarch.cfg.leapstergs 2> /root/retro.log
+  retroarch -v -c $CONFIG 2> /root/retro.log
   echo "Restarting program..."
 done
 
