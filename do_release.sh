6f80b73f45e7b8366a28fc14e5bdc9e4658a91c5
mac2612 <jadedstupidandreckless@gmail.com>
2020-05-10 14:23:22 -0600

    Add additional release items

--- do_release.sh
+++ do_release.sh
@@ -1,8 +1,16 @@
-# lf2000 surgeon
+# TODO: Do something when one of these steps fails instead of just moving on.
+# lf1000 surgeon
 make clean
 make lf1000_surgeon_defconfig
 make
 cp output/images/zImage release/lf1000_surgeon_zImage
+
+# lf1000 main
+make clean
+make lf1000_defconfig
+make
+cp output/images/zImage release/lf1000_zImage
+cp output/images/rootfs.tar.gz release/lf1000_rootfs.tar.gz
 
 # lf2000 surgeon
 make clean
@@ -16,6 +24,13 @@ make lf2000_defconfig
 make
 cp output/images/uImage release/lf2000_uImage
 cp output/images/rootfs.tar.gz release/lf2000_rootfs.tar.gz
+
+
+# lf3000 surgeon
+make clean
+make lf3000_surgeon_defconfig
+make
+cp output/images/zImage release/lf3000_surgeon_zImage
 
 # lf3000 main
 make clean
