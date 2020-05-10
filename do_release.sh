405321654d705acefaeb6b0cd85e03bc0a00bb81
mac2612 <jadedstupidandreckless@gmail.com>
2020-05-10 12:58:40 -0600

    lf3000 release

--- do_release.sh
+++ do_release.sh
@@ -10,3 +10,11 @@ make lf2000_defconfig
 make
 cp output/images/uImage release/lf2000_uImage
 cp output/images/rootfs.tar.gz release/lf2000_rootfs.tar.gz
+
+# lf3000 main
+make clean
+make lf3000_defconfig
+make
+cp output/images/rootfs.tar.gz release/lf3000_rootfs.tar.gz
+cp output/images/uImage release/lf3000_uImage.tar.gz
+
