66aa9c3b8e4e7d86f5c02af3b420fafca62ee4b0
mac2612 <jadedstupidandreckless@gmail.com>
2021-03-21 22:41:44 -0400

    genericize menu respawn logic to include gmenu

---
+++ board/leapfrog/lfx000/fsoverlay/usr/bin/respawn_menu.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CONFIG=/configs/retroarch.cfg.leapstergs
+
+valencia=$(fbset | grep 480x272)
+if [[ $? == 0 ]]; then
+    CONFIG=/configs/retroarch.cfg.leappad2
+fi
+
+rio=$(fbset | grep 1024x600)
+if [[ $? == 0 ]]; then
+    CONFIG=/configs/retroarch.cfg.leappadultra
+fi
+
+# If gmenunx is present, run it instead of retroarch.
+# TODO: Fix this to be more graceful across different devices etc.
+if test -f "/usr/share/gmenunx/gmenunx"
+then
+	/usr/share/gmenunx/./gmenunx
+else
+
+	while `true`
+	do
+	  echo 0 > /sys/devices/platform/lf2000-fb.0/graphics/fb0/blank
+	  export SDL_NOMOUSE=1
+	  retroarch -v -c $CONFIG 2> /root/retro.log
+	  echo "Restarting program..."
+	done
+fi
