6643bc62743d55e0e394544d2f136678e7bb9421
mac2612 <jadedstupidandreckless@gmail.com>
2021-03-26 22:59:29 -0400

    Copy in ohboy config and symlink

--- board/leapfrog/lfx000/configs_fixup.sh
+++ board/leapfrog/lfx000/configs_fixup.sh
@@ -6,4 +6,18 @@ if test -f "$1/usr/share/gmenunx/gmenunx"; then
 		cp board/leapfrog/lfx000/configs/gmenunx/menuitems/picodrive $1/.gmenunx/sections/emulators/picodrive
 		echo "adding picodrive icon"
 	fi
+	if test -f "$1/usr/bin/PocketSNES"; then
+		cp board/leapfrog/lfx000/configs/gmenunx/menuitems/pocketsnes $1/.gmenunx/sections/emulators/pocketsnes
+		echo "adding pocketsnes icon"
+	fi
+	if test -f "$1/usr/bin/ohboy"; then
+		cp board/leapfrog/lfx000/configs/gmenunx/menuitems/ohboy $1/.gmenunx/sections/emulators/ohboy
+		mkdir -p $1/configs/ohboy
+		cp board/leapfrog/lfx000/configs/ohboy/* $1/configs/ohboy/
+		#Crummy hack, fix this when we have a menuing system that can change the working directory.
+		ln -s /usr/bin/ohboy $1/configs/ohboy/ohboy
+		echo "adding ohboy icon + configs"
+	fi
+
+
 fi
