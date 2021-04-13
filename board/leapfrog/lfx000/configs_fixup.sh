595decd6725efdd1f700185de2b4ba57555f73c9
mac2612 <jadedstupidandreckless@gmail.com>
2021-04-13 00:00:44 -0400

    Fix typo in configs fixup

--- board/leapfrog/lfx000/configs_fixup.sh
+++ board/leapfrog/lfx000/configs_fixup.sh
@@ -2,6 +2,9 @@
 if test -f "$1/usr/share/gmenunx/gmenunx"; then
 	echo "gmenunx found!"
 	mkdir -p $1/.gmenunx/sections/emulators
+	# gmenunx defaults, mostly for default volume setting.
+	cp board/leapfrog/lfx000/configs/gmenunx/gmenunx.conf $1/.gmenunx/gmenunx.conf
+
 	if test -f "$1/usr/bin/PicoDrive"; then
 		cp board/leapfrog/lfx000/configs/gmenunx/menuitems/picodrive $1/.gmenunx/sections/emulators/picodrive
 		echo "adding picodrive icon"
