a8af299595de41c6a29fe34521b0ff813811888d
natesway <64743334+natesway@users.noreply.github.com>
2022-11-04 22:39:52 -0400

    Update do_release.sh

--- do_release.sh
+++ do_release.sh
@@ -1,32 +1,3 @@
-# TODO: Do something when one of these steps fails instead of just moving on.
-
-
-# lf1000_didj surgeon
-make clean
-make lf1000_didj_surgeon_defconfig
-make
-cp output/images/zImage release/lf1000_didj_surgeon_zImage
-
-# lf1000 main
-make clean
-make lf1000_didj_defconfig
-make
-cp output/images/zImage release/lf1000_didj_zImage
-cp output/images/rootfs.tar.gz release/lf1000_didj_rootfs.tar.gz
-
-# lf1000 surgeon
-make clean
-make lf1000_surgeon_defconfig
-make
-cp output/images/zImage release/lf1000_surgeon_zImage
-
-# lf1000 main
-make clean
-make lf1000_defconfig
-make
-cp output/images/zImage release/lf1000_zImage
-cp output/images/rootfs.tar.gz release/lf1000_rootfs.tar.gz
-
 # lf2000 surgeon
 make clean
 make lf2000_surgeon_defconfig
@@ -39,18 +10,4 @@ make lf2000_defconfig
 make
 cp output/images/uImage release/lf2000_uImage
 cp output/images/rootfs.tar.gz release/lf2000_rootfs.tar.gz
-
-
-# lf3000 surgeon
-make clean
-make lf3000_surgeon_defconfig
-make
-cp output/images/zImage release/lf3000_surgeon_zImage
-
-# lf3000 main
-make clean
-make lf3000_defconfig
-make
-cp output/images/rootfs.tar.gz release/lf3000_rootfs.tar.gz
-cp output/images/uImage release/lf3000_uImage.tar.gz
 
